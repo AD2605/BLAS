ad9bcbe6277597f1acddc8595e764a9348939a87
AD2605 <atharvadubey26@gmail.com>
2021-06-24 20:14:22 +0530

    Added Copy Kernels

--- MatrixOps.cu
+++ MatrixOps.cu
@@ -14,6 +14,16 @@ namespace blas1{
         }
     }
 
+    __global__ void naiveCopy(float* input, float* output, size_t width, size_t height){
+
+        unsigned int row_id = blockIdx.y * blockDim.y + threadIdx.y;
+        unsigned int column_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+        if((row_id < height) && (column_id < width)){
+            output[row_id * width + column_id] = input[row_id * width + column_id];
+        }
+    }
+
 
     __global__ void transpose_32(float* input, float* output, size_t width, size_t height){
         const unsigned int BLOCK_SIZE = 32;
@@ -86,4 +96,79 @@ namespace blas1{
             output[ (local_column + j) * w + local_row] = tile[threadIdx.x][threadIdx.y + j];
         }
     }
+
+
+    __global__ void transposeRowBlock_16(float* input, float* output, size_t width, size_t heights){
+        const unsigned int TILE_SIZE = 16;
+        const unsigned int BLOCK_ROWS = 8;
+
+        __shared__ float tile[TILE_SIZE][TILE_SIZE+1];
+
+        unsigned int x = blockIdx.x * TILE_SIZE + threadIdx.x;
+        unsigned int y = blockIdx.y * TILE_SIZE + threadIdx.y;
+        auto w = gridDim.x * TILE_SIZE;
+
+        for(int j=0; j<TILE_SIZE; j += BLOCK_ROWS){
+            tile[threadIdx.y + j][threadIdx.x] = input[(y + j) * w + x];
+        }
+        __syncthreads();
+
+        auto local_row = blockIdx.y * TILE_SIZE + threadIdx.x;
+        auto local_column = blockIdx.x * TILE_SIZE + threadIdx.y;
+
+#pragma unroll
+        for(int j = 0; j < TILE_SIZE; j += BLOCK_ROWS){
+            output[ (local_column + j) * w + local_row] = tile[threadIdx.x][threadIdx.y + j];
+        }
+    }
+
+    __global__ void MatrixCopy_32(float* input, float* output, size_t width, size_t height){
+        const unsigned int BLOCK_SIZE = 32;
+
+        __shared__ float sharedMem[BLOCK_SIZE][BLOCK_SIZE + 1]; // Bank conflict
+
+        unsigned int column_index = blockIdx.x * blockDim.x + threadIdx.x;
+        unsigned int row_index = blockIdx.y * blockDim.y + threadIdx.y;
+
+        if((column_index < width) && (row_index < height)){
+
+            unsigned element_index = row_index * width + column_index;
+            sharedMem[threadIdx.y][threadIdx.x] = input[element_index];
+        }
+
+        __syncthreads();
+
+        unsigned int local_row = blockIdx.y * BLOCK_SIZE + threadIdx.x;  // coalesced access
+        unsigned int local_column = blockIdx.x * BLOCK_SIZE + threadIdx.y; // coalesced access
+
+        if((local_row < height) && (local_column < width)){
+            output[local_row * width + local_column] = sharedMem[threadIdx.x][threadIdx.y];
+        }
+    }
+
+
+    __global__ void MatrixCopy_16(float* input, float* output, size_t width, size_t height){
+        const unsigned int BLOCK_SIZE = 32;
+
+        __shared__ float sharedMem[BLOCK_SIZE][BLOCK_SIZE + 1]; // Bank conflict solution
+
+        unsigned int column_index = blockIdx.x * blockDim.x + threadIdx.x;
+        unsigned int row_index = blockIdx.y * blockDim.y + threadIdx.y;
+
+        if((column_index < width) && (row_index < height)){
+
+            unsigned element_index = row_index * width + column_index;
+            sharedMem[threadIdx.y][threadIdx.x] = input[element_index];
+        }
+
+        __syncthreads();
+
+        unsigned int local_row = blockIdx.y * BLOCK_SIZE + threadIdx.x;  // coalesced access
+        unsigned int local_column = blockIdx.x * BLOCK_SIZE + threadIdx.y; // coalesced access
+
+        if((local_row < height) && (local_column < width)){
+            output[local_row * width + local_column] = sharedMem[threadIdx.x][threadIdx.y];
+        }
+    }
+
 }
