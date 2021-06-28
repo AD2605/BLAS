f6b8ace061f63d6e3c881a751427ec77ce4424e5
Atharva Dubey <atharvadubey26@gmail.com>
2021-06-28 14:07:27 +0530

    Delete vectorOps.cuh

--- vectorOps.cuh
+++
@@ -1,9 +0,0 @@
-#ifndef MATRIXMULTIPLICATION_VECTOROPS_CUH
-#define MATRIXMULTIPLICATION_VECTOROPS_CUH
-
-
-namespace blas1{
-    float sdot(float* vectorA, float* vectorB, size_t numElements);
-}
-
-#endif //MATRIXMULTIPLICATION_VECTOROPS_CUH
